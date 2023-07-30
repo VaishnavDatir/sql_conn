bd52f2fc11d5d48090fd448abd07eca587def197
Vaishnav Datir <vaishnav.datir@gmail.com>
2023-07-31 00:10:43 +0530

    initial v1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,10 +5,12 @@ import 'package:sql_conn/sql_conn.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
